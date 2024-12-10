5cbbaa48e2aebec464e651ff0232dfc975ceb988
Zev <44043800+devlt0@users.noreply.github.com>
2024-12-10 01:06:22 -0500

    fix typoes prior to testing draft

--- 5_Performance_Testing/bdd_load_testing/features/steps/test_login_registration_steps.py
+++ 5_Performance_Testing/bdd_load_testing/features/steps/test_login_registration_steps.py
@@ -4,13 +4,13 @@ import requests
 
 BASE_URL = "http://localhost:5000"
 
-@when('When load tests are executed using "{num_users}" users for "{load_duration}" minutes spawn rate "{spawn_rate}" per sec and output report "{report_fname}"')
+@when('load tests are executed using "{num_users}" users for "{load_duration}" minutes spawn rate "{spawn_rate}" per sec and output report "{report_fname}"')
 def step_impl(context, num_users, load_duration, spawn_rate, report_fname):
     if not report_fname.endswith(".html"):
         report_fname += '.html'
     print("Executing Locust load tests...")
     result = subprocess.run([
-        "locust", "-f", "locust_login_registration_mixedvalid.py", "--headless", "--users", f"{num_users}", "--spawn-rate", f"{spawn_rate}", "--run-time", "{load_duration}m", "--html", f"{report_fname}"
+        "locust", "-f", "C:/Users/human-c137/Documents/GitHub/soar_qa_assessment/5_Performance_Testing/bdd_load_testing/features/steps/locust_login_registration_mixedvalid.py", "--headless", "--users", f"{num_users}", "--spawn-rate", f"{spawn_rate}", "--run-time", f"{load_duration}m", "--html", f"{report_fname}"
     ], capture_output=True, text=True)
     context.locust_result = result
     print(result.stdout)
